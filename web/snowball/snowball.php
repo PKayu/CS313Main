f45f0ff5d3dc6a777aaf81ef93fec5674c6c3dd0
Dan Worwood <danworwood@gmail.com>
2019-10-15 09:26:24 -0500

    Connect to server

--- web/snowball/snowball.php
+++ web/snowball/snowball.php
@@ -25,6 +25,7 @@ include "header.php";
             $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo 'Successfully Connected!';
         }
         catch (PDOException $ex)
         {
