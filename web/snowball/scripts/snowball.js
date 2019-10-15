e8a3ad08c8a9cd970253f1ad3bc3063c350919cc
Dan Worwood <danworwood@gmail.com>
2019-10-15 14:53:48 -0500

    Select Query test

--- web/snowball/scripts/snowball.js
+++ web/snowball/scripts/snowball.js
@@ -18,8 +18,8 @@
 function calculate() {
 	var remaining_amounts = document.getElementsByClassName("remaining_amount");
 	var debtRemaining = 0;
-	for (var index = 1; index < remaining_amounts.length; index++) {
-		debtRemaining += remaining_amounts[index].value;
+	for (var index = 0; index < remaining_amounts.length; index++) {
+		debtRemaining += parseFloat(remaining_amounts[index].value);
 	}
 	document.getElementById("sumAmount").value = debtRemaining;
 }
