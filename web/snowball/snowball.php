9b26ec8f381aaa1e059f2151cff10ea8dcc38278
Dan Worwood <danworwood@gmail.com>
2019-10-15 11:51:23 -0500

    Select Query test

--- web/snowball/snowball.php
+++ web/snowball/snowball.php
@@ -1,49 +1,52 @@
 <?php
-$title = "Home";
+$title = "Snowball Calculator";
 $css = "snowball.css";
 $javascript = "snowball.js";
 include "header.php";
 ?>
+<?php
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
 
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Successfully Connected!<br/>';
+    $id = 0;
+    $stmt = $db->prepare('SELECT * FROM "Snowball"."Users" WHERE id=:id');
+    $stmt->execute(array(':id' => $id));
+    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+?>
 <!-- Begin Page Content -->
 <h2>BYU CS313</h2>
 <div class="flexer">
     <div>
-        <?php
-        try
-        {
-            $dbUrl = getenv('DATABASE_URL');
-
-            $dbOpts = parse_url($dbUrl);
-
-            $dbHost = $dbOpts["host"];
-            $dbPort = $dbOpts["port"];
-            $dbUser = $dbOpts["user"];
-            $dbPassword = $dbOpts["pass"];
-            $dbName = ltrim($dbOpts["path"],'/');
-
-            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo 'Successfully Connected!';
-
-            foreach ($db->query('SELECT * FROM "Snowball"."Users"') as $row)
-            {
-                echo 'user ID: ' . $row['user_id'];
-                echo ' <br/> Name: ' . $row['first_name'] . ' ' . $row['last_name'];
-                echo ' <br/> Additional Funds: $' . $row['addit_funds'];
-                echo '<br/>';
-            }
-        }
-        catch (PDOException $ex)
-        {
-            echo 'Error!: ' . $ex->getMessage();
-            die();
-        }
-        ?>
+        <h1>Snowball Calculator</h1>
+        <p>
+            Want to know the quickest way to pay off your debt? By entering in your remaining debt below, our calculator
+            will show you where to ues your money in order to pay of debt as quickly as possible. This will save you from paying interest
+            on your loans and will reduce the amount of time you stay in debt.
+        </p>
     </div>
     <div>
 
+        <h2><?php $user['first_name']. ' ' . $user['last_name']?></h2>
     </div>
 
 </div>
