e3d57301595a4dab718dc9ead23e713388a09e27
Dan Worwood <danworwood@gmail.com>
2019-10-26 12:00:29 -0500

    update registration page

--- web/snowball/registered.php
+++ web/snowball/registered.php
@@ -15,16 +15,17 @@ try
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'connect to db <br>';
+    echo 'connected to db <br>';
     $query = 'INSERT INTO "Snowball"."Users" VALUES (:id, :firstName, :lastName, :password)';
     $statement = $db->prepare($query);
 
     $userGuid = com_create_guid();
+    echo 'created GUID: '. $userGuid.'<br>';
     $firstName = $_POST["f_name"];
     $lastName = $_POST["l_name"];
     $password = $_POST["password"];
 
-    echo 'load variables'. $userGuid.', '.$firstName.', '.$lastName.', '.$password.' <br>';
+    echo 'load variables, '.$firstName.', '.$lastName.', '.$password.' <br>';
 
     $statement->bindValue(':id', $userGuid);
     $statement->bindValue(':firstName', $firstName);
