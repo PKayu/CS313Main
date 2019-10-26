1d7fa3c5fd6745911dadb0e93aea605b430b0d27
Dan Worwood <danworwood@gmail.com>
2019-10-26 12:02:40 -0500

    update registration page

--- web/snowball/registered.php
+++ web/snowball/registered.php
@@ -1,5 +1,24 @@
 <?php
 echo 'load page<br>';
+function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }
+    else {
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
+    }
+}
+
 try
 {
     $dbUrl = getenv('DATABASE_URL');
@@ -19,7 +38,7 @@ try
     $query = 'INSERT INTO "Snowball"."Users" VALUES (:id, :firstName, :lastName, :password)';
     $statement = $db->prepare($query);
 
-    $userGuid = com_create_guid();
+    $userGuid = getGUID();
     echo 'created GUID: '. $userGuid.'<br>';
     $firstName = $_POST["f_name"];
     $lastName = $_POST["l_name"];
