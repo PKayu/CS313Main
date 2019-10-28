f88a6f454a70a605113f985637cb982fe0a5e43e
Dan Worwood <danworwood@gmail.com>
2019-10-28 12:09:42 -0500

    insert into database

--- web/snowball/login.php
+++ web/snowball/login.php
@@ -26,6 +26,20 @@ session_start();
                 if (empty($_SESSION["user"])) {
                     try
                     {
+                        $dbUrl = getenv('DATABASE_URL');
+
+                        $dbOpts = parse_url($dbUrl);
+
+                        $dbHost = $dbOpts["host"];
+                        $dbPort = $dbOpts["port"];
+                        $dbUser = $dbOpts["user"];
+                        $dbPassword = $dbOpts["pass"];
+                        $dbName = ltrim($dbOpts["path"],'/');
+
+                        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        
                         $username = $_POST["username"];
                         $password = $_POST["password"];
 
