3f5f40ebf89c8c373b1dd1df1efe106672aaae2f
Dan Worwood <danworwood@gmail.com>
2019-10-28 11:10:33 -0500

    insert into database

--- web/snowball/save.php
+++ web/snowball/save.php
@@ -20,6 +20,8 @@ try
     $debt_id = $_POST['id'];
     echo 'debt id: ' . $debt_id;
 
+    $debt_id = $_SESSION['user'][0]["user_id"];
+
     $user_id = $_SESSION['user'][0]["user_id"];
     $userQuery = 'INSERT INTO "Snowball"."Users" (debt_id, fk_user_id) VALUES (:id, :userid)';
     $statement = $db->prepare($userQuery);
