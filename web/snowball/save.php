78e3eaff70438005649ed692cd37d6136520d64c
Dan Worwood <danworwood@gmail.com>
2019-10-28 11:54:06 -0500

    insert into database

--- web/snowball/save.php
+++ web/snowball/save.php
@@ -1,23 +1,9 @@
 <?php
+include 'connection.php';
 echo 'load page<br>';
 session_start();
 try
 {
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'connected to db <br>';
-
     $debt_id = $_POST['id'];
     echo 'debt id: ' . $debt_id;
 
